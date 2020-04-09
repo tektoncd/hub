7b04937ffbc874225c48cb78ec7aeb4089e5bafb
PuneetPunamiya <puneetpunamiya@gmail.com>
2020-04-09 09:57:01 +0530

    Adds skeleton for Hub Api using GOA framework
    The patch includes:
      - a design package that contains the `goa` api design
      - code  generated using `goa gen` tool from the design package
    Fixes: #2
    
    Signed-off-by: Puneet Punamiya <ppunamiy@redhat.com>

---
+++ api/design/design.go
@@ -0,0 +1,26 @@
+package design
+import (
+	. "goa.design/goa/v3/dsl"
+)
+var _ = API("api_server", func() {
+	Title("Tekton Hub")
+	Description("Service to get resource details")
+    Server("api", func() {
+        Host("localhost", func() {
+            URI("http://localhost:8000")
+        })
+    })
+})
+var _ = Service("api", func() {
+	Description("The api service gives resource details")
+
+	//Method to get all resources
+	Method("list", func() {
+		Description("Get all tasks and pipelines.")
+		HTTP(func() {
+			GET("/resources")
+			Response(StatusOK)
+		})
+	})
+	Files("/openapi.json", "./gen/http/openapi.json")
+})
