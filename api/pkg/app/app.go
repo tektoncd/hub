eee383da8e608146887ff89690998600a63349ad
pratap0007 <shverma@redhat.com>
2020-06-29 14:15:38 +0530

    Adds gorm connection

--- api/pkg/app/app.go
+++ api/pkg/app/app.go
@@ -1,15 +1,22 @@
 package app
 
 import (
-	"strings"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"os"
+	"strings"
 
+	// Blank for package side effect: loads postgres drivers
+	_ "github.com/lib/pq"
 )
 
 type Base interface {
 	Environment() EnvMode
 	Logger() *zap.SugaredLogger
+	Databse() *Database
 	Cleanup()
 }
 
@@ -25,11 +32,32 @@ const (
 	Test        EnvMode = "test"
 )
 
+type Database struct {
+	Host     string
+	Port     string
+	Name     string
+	User     string
+	Password string
+}
+
+func (db *Database) String() string {
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s password=xxxxxx dbname=%s sslmode=disable",
+		db.Host, db.Port, db.User, db.Name)
+}
+
+func (db *Database) ConnectionString() string {
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		db.Host, db.Port, db.User, db.Password, db.Name)
+}
+
 type BaseConfig struct {
 	mode   EnvMode
 	logger *zap.SugaredLogger
+	dbConf *Database
+	db     *gorm.DB
 }
-
 
 func (bc *BaseConfig) Environment() EnvMode {
 	return bc.mode
@@ -41,13 +69,29 @@ func (bc *BaseConfig) Logger() *zap.SugaredLogger {
 
 func (bc *BaseConfig) Cleanup() {
 	bc.logger.Sync()
+	bc.db.Close()
+}
+
+func (bc *BaseConfig) Database() *Database {
+	return bc.dbConf
+}
+
+func (bc *BaseConfig) DB() *gorm.DB {
+	return bc.db
 }
 
 type ApiConfig struct {
 	*BaseConfig
 }
+type TestConfig struct {
+	*BaseConfig
+}
 
 func BaseConfigFromEnv() (*BaseConfig, error) {
+	// load from .env file but skip if not found
+	if err := godotenv.Load(); err != nil {
+		fmt.Fprintf(os.Stdout, "SKIP: loading .ApiConfig failed: %s", err)
+	}
 	mode := Environment()
 	var err error
 
@@ -58,7 +102,17 @@ func BaseConfigFromEnv() (*BaseConfig, error) {
 
 	log.With("name", "app").Infof("in %q mode ", mode)
 
-	 bc := &BaseConfig{mode: mode, logger: log}
+	bc := &BaseConfig{mode: mode, logger: log}
+	if bc.dbConf, err = initDB(mode); err != nil {
+		log.Error(err, "failed to obtain database configuration")
+		return nil, err
+	}
+	bc.db, err = gorm.Open("postgres", bc.dbConf.ConnectionString())
+	if err != nil {
+		log.Error(err, "failed to establish database connection")
+		return nil, err
+	}
+	log.Infof("Successfully connected to db %s", bc.dbConf)
 	return bc, nil
 }
 
@@ -72,9 +126,11 @@ func FromEnv() (*ApiConfig, error) {
 	return ApiConfig, nil
 }
 
-
 func Environment() EnvMode {
 	mode := "development"
+	if val, ok := os.LookupEnv("ENVIRONMENT"); ok {
+		mode = val
+	}
 
 	switch strings.ToLower(mode) {
 	case "development":
@@ -84,6 +140,35 @@ func Environment() EnvMode {
 	default:
 		return Production
 	}
+}
+
+func initDB(mode EnvMode) (*Database, error) {
+	var err error
+	db := &Database{}
+	if db.Host, err = env("POSTGRESQL_HOST"); err != nil {
+		return nil, err
+	}
+	if db.Port, err = env("POSTGRESQL_PORT"); err != nil {
+		return nil, err
+	}
+	if db.Name, err = env("POSTGRESQL_DATABASE"); err != nil {
+		return nil, err
+	}
+	if db.User, err = env("POSTGRESQL_USER"); err != nil {
+		return nil, err
+	}
+	if db.Password, err = env("POSTGRESQL_PASSWORD"); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func env(key string) (string, error) {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return "", fmt.Errorf("NO %q environment variable defined", key)
+	}
+	return val, nil
 }
 
 func initLogger(mode EnvMode) (*zap.SugaredLogger, error) {
--- api/pkg/service/category.go
+++ api/pkg/service/category.go
@@ -2,20 +2,23 @@ package hub
 
 import (
 	"context"
+	"github.com/jinzhu/gorm"
 	"go.uber.org/zap"
 
 	category "github.com/tektoncd/hub/api/gen/category"
+	app "github.com/tektoncd/hub/api/pkg/app"
 )
 
 // category service example implementation.
 // The example methods log the requests and return zero values.
 type categorysrvc struct {
 	logger *zap.SugaredLogger
+	db     *gorm.DB
 }
 
 // NewCategory returns the category service implementation.
-func NewCategory(logger *zap.SugaredLogger) category.Service {
-	return &categorysrvc{logger}
+func NewCategory(api *app.ApiConfig) category.Service {
+	return &categorysrvc{api.Logger(), api.DB()}
 }
 
 // Get all Categories with their tags sorted by name
