05de2e5b0c9b6e344325e5bfba36a07ceb4383ab
Sunil Thaha <sthaha@redhat.com>
2020-07-22 18:00:04 +1000

    Versions by resource id Api implementation and tests
    This adds the api implementation which allows to find all
    versions of a resource using resource id.
    Endpoint: /resource/<id>/versions
    
    Closes: #13
    
    Signed-off-by: Shivam Mukhade <smukhade@redhat.com>

--- api/pkg/service/resource/resource.go
+++ api/pkg/service/resource/resource.go
@@ -74,16 +74,33 @@ func (s *service) resourcesForQuery(q *gorm.DB) (resource.ResourceCollection, er
 	return res, nil
 }
 
-// withResourceDetails defines a gorm scope to include all details of resource.
-func withResourceDetails(db *gorm.DB) *gorm.DB {
-	return db.Order("rating DESC, name").
-		Preload("Catalog").
-		Preload("Versions", func(db *gorm.DB) *gorm.DB {
-			return db.Order("string_to_array(version, '.')::int[];")
-		}).
-		Preload("Tags", func(db *gorm.DB) *gorm.DB {
-			return db.Order("tags.name ASC")
-		})
+// VersionsByID returns all versions of a resource given its resource id
+func (s *service) VersionsByID(ctx context.Context, p *resource.VersionsByIDPayload) (res *resource.Versions, err error) {
+
+	q := s.db.Scopes(orderByVersion).Where("resource_id = ?", p.ID)
+
+	var all []model.ResourceVersion
+	if err := q.Find(&all).Error; err != nil {
+		s.logger.Error(err)
+		return nil, fetchError
+	}
+
+	if len(all) == 0 {
+		return nil, notFoundError
+	}
+
+	var allVersions []*resource.Version
+	for _, r := range all {
+		allVersions = append(allVersions, initResourceVersion(r))
+	}
+	latestVersion := initResourceVersion(all[len(all)-1])
+
+	res = &resource.Versions{
+		Latest:   latestVersion,
+		Versions: allVersions,
+	}
+
+	return res, nil
 }
 
 func initResource(r model.Resource) *resource.Resource {
@@ -98,7 +115,7 @@ func initResource(r model.Resource) *resource.Resource {
 	res.Rating = r.Rating
 
 	lv := (r.Versions)[len(r.Versions)-1]
-	res.LatestVersion = &resource.Version{
+	res.LatestVersion = &resource.LatestVersion{
 		ID:                  lv.ID,
 		Version:             lv.Version,
 		Description:         lv.Description,
@@ -116,4 +133,29 @@ func initResource(r model.Resource) *resource.Resource {
 	}
 
 	return res
+}
+
+func initResourceVersion(r model.ResourceVersion) *resource.Version {
+	res := &resource.Version{
+		ID:      r.ID,
+		Version: r.Version,
+		WebURL:  r.URL,
+		RawURL:  replaceStrings.Replace(r.URL),
+	}
+
+	return res
+}
+
+// withResourceDetails defines a gorm scope to include all details of resource.
+func withResourceDetails(db *gorm.DB) *gorm.DB {
+	return db.Order("rating DESC, name").
+		Preload("Catalog").
+		Preload("Versions", orderByVersion).
+		Preload("Tags", func(db *gorm.DB) *gorm.DB {
+			return db.Order("tags.name ASC")
+		})
+}
+
+func orderByVersion(db *gorm.DB) *gorm.DB {
+	return db.Order("string_to_array(version, '.')::int[];")
 }
