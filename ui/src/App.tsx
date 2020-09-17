fab33970634cb0093fae0fe7beec66e9d6826237
PuneetPunamiya <puneetpunamiya@gmail.com>
2020-09-17 09:15:19 +0530

    Fix eslint/prettier warnings and errors
    Signed-off-by: Puneet Punamiya <ppunamiy@redhat.com>

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +21,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
