64b1ebe15fb9b39d9aebfef5c8d3a43dd640ccdb
PuneetPunamiya <puneetpunamiya@gmail.com>
2020-12-23 23:23:18 +0530

    Adds Env vars as application config
    Signed-off-by: Puneet Punamiya <ppunamiy@redhat.com>

---
+++ ui/image/start.sh
@@ -0,0 +1,29 @@
+#!//bin/bash
+set -e -u -o pipefail
+
+CONFIG_JS=/usr/share/nginx/html/config.js
+echo "Current ENV"
+echo '----------------------------------------------'
+cat $CONFIG_JS
+echo '..............................................'
+ls -l  $CONFIG_JS
+echo "whoami: $(id)"
+echo '----------------------------------------------'
+
+cat <<-EOF > $CONFIG_JS
+window.config = {
+  API_URL: '$API_URL',
+  GH_CLIENT_ID: '$GH_CLIENT_ID',
+};
+
+
+EOF
+
+echo "Modified ENV"
+cat $CONFIG_JS
+echo '----------------------------------------------'
+
+
+echo Starting Nginx
+exec nginx -g 'daemon off;'
+
