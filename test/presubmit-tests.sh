9eb4cd892e201facc092cfdafe89eeca4fdb4438
PuneetPunamiya <puneetpunamiya@gmail.com>
2021-02-24 10:18:59 +0530

    Updates CI by adding golangci-lint and yamllint checks
    Signed-off-by: Puneet Punamiya <ppunamiy@redhat.com>

--- test/presubmit-tests.sh
+++ test/presubmit-tests.sh
@@ -85,8 +85,20 @@ api-unittest(){
   info Running unittests
 
   go mod vendor
-  go test -p 1 -v ./pkg/... && 
+  go test -p 1 -v ./pkg/... &&
   go test -p 1 -v ./v1/service/...
+}
+
+api-golangci-lint() {
+  info Running go lint
+
+  golangci-lint run ./pkg/... ./v1/service/...
+}
+
+yaml-lint() {
+  info Running Yamllint
+
+  yamllint -c .yamllint ./config.yaml ./config
 }
 
 api-build(){
@@ -122,12 +134,18 @@ run_unit_tests() {
 
     cd $API_DIR
     api-unittest
+    api-golangci-lint
   )
   (
     set -eu -o pipefail
 
     cd $UI_DIR
     ui-unittest
+  )
+  (
+    set -eu -o pipefail
+
+    yaml-lint
   )
 }
 
