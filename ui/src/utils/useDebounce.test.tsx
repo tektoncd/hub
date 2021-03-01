cc8c3eb8f8e464ee06c0ddce8a79da6ac6ac8d33
PuneetPunamiya <puneetpunamiya@gmail.com>
2021-03-01 11:52:24 +0530

    UI: Minor fixes for updating the packages and fixing lint
    Signed-off-by: Puneet Punamiya <ppunamiy@redhat.com>

--- ui/src/utils/useDebounce.test.tsx
+++ ui/src/utils/useDebounce.test.tsx
@@ -1,17 +1,27 @@
 import { renderHook } from '@testing-library/react-hooks';
+import { when } from 'mobx';
 import { FakeHub } from '../api/testutil';
 import { createProviderAndStore } from '../store/root';
 import { useDebounce } from './useDebounce';
 
 const TESTDATA_DIR = `src/store/testdata`;
 const api = new FakeHub(TESTDATA_DIR);
-const {} = createProviderAndStore(api);
+const { root } = createProviderAndStore(api);
 
 describe('useDebounce', () => {
   it('it renders useDebounce', (done) => {
-    const { result } = renderHook(() => useDebounce('cli', 400));
-    expect(result.current).toBe('cli');
-
-    done();
+    const { resources } = root;
+    when(
+      () => {
+        return !resources.isLoading;
+      },
+      () => {
+        setTimeout(() => {
+          const { result } = renderHook(() => useDebounce('cli', 400));
+          expect(result.current).toBe('cli');
+          done();
+        }, 0);
+      }
+    );
   });
 });
