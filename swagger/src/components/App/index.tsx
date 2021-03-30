7b363c786bd02184fbee21bb5a0619ac2111bca7
PuneetPunamiya <puneetpunamiya@gmail.com>
2021-03-30 22:57:11 +0530

    Adds Swagger UI for Hub API
    Signed-off-by: Puneet Punamiya <ppunamiy@redhat.com>

--- swagger/src/components/App/index.tsx
+++ swagger/src/components/App/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import SwaggerUI from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css';
+import { API_URL } from '../../config/constants';
 
-const App = () => {
-  return <div></div>;
+const App: React.FC = () => {
+  return (
+    <div>
+      <SwaggerUI url={API_URL} />
+    </div>
+  );
 };
 
 export default App;
