0d3fe447ae1a290369c0d936e78e2480680b96a1
pratap0007 <shverma@redhat.com>
2021-05-03 16:06:02 +0530

    Adds a keyboard shortcut `/` to focus on search bar
    Signed-off-by: Shiv Verma <shverma@redhat.com>

--- ui/src/containers/Search/index.tsx
+++ ui/src/containers/Search/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { useObserver } from 'mobx-react';
+import { useHotkeys } from 'react-hotkeys-hook';
 import { TextInput } from '@patternfly/react-core';
 import { useMst } from '../../store/root';
 import { useDebounce } from '../../utils/useDebounce';
+import { assert } from '../../store/utils';
 import './Search.css';
 
 const Search: React.FC = () => {
+  useHotkeys('/', () => {
+    // Without timeout the hotkey will be sent to select input
+    setTimeout(() => {
+      const currentInput = inputRef.current;
+      assert(currentInput);
+      currentInput.focus();
+    }, 20);
+  });
+
   const { resources } = useMst();
+
+  const inputRef = React.useRef<HTMLInputElement>(null);
 
   const onSearchChange = useDebounce(resources.search, 400);
 
@@ -22,6 +35,7 @@ const Search: React.FC = () => {
 
   return useObserver(() => (
     <TextInput
+      ref={inputRef}
       value={resources.search}
       type="search"
       onChange={(resourceName: string) => {
