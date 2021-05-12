e85d9015492462acfb90c4d1111f03ffe2a6e94d
PuneetPunamiya <puneetpunamiya@gmail.com>
2021-05-12 10:29:36 +0530

    Adds interactive mode for info command
      - This patch interactively ask the users to select
    
    	- Catalog first
       	- Resources in the catalog selected
    	- All Versions of the resource selected
    
      - If there is only one catalog or version then
        it will be auto-selected
    
      - User can also get the information of the resource
        if the user know the catalog name, resource name
        and version of the resource by
    
        `tkn hub info task <task-name> --version <version-number> --from catalog <catalog-name>`
    
    Signed-off-by: Puneet Punamiya <ppunamiy@redhat.com>

--- api/pkg/cli/cmd/info/info_test.go
+++ api/pkg/cli/cmd/info/info_test.go
@@ -22,157 +22,237 @@ import (
 	"github.com/stretchr/testify/assert"
 	res "github.com/tektoncd/hub/api/gen/resource"
 	"github.com/tektoncd/hub/api/pkg/cli/test"
+	cat "github.com/tektoncd/hub/api/v1/gen/catalog"
 	"gopkg.in/h2non/gock.v1"
 	"gotest.tools/v3/golden"
 )
 
-var resource = &res.ResourceData{
-	ID:   1,
-	Name: "foo",
+type InfoOptions struct {
+	ResId   int
+	Name    string
+	Kind    string
+	Catalog string
+	Version string
+}
+
+var catalogs = &cat.ListResult{
+	Data: []*cat.Catalog{
+		{
+			ID:   1,
+			Name: "tekton",
+			Type: "community",
+			URL:  "https://github.com/tektoncd/catalog",
+		},
+		{
+			ID:   2,
+			Name: "fruit",
+			Type: "community",
+			URL:  "https://github.com/fruit/catalog",
+		},
+	},
+}
+
+var res2 = &res.ResourceData{
+	ID:   2,
+	Name: "foo-bar",
 	Kind: "Task",
 	Catalog: &res.Catalog{
 		ID:   1,
 		Name: "tekton",
 		Type: "community",
 	},
-	Rating: 4.8,
+	Rating: 4,
 	LatestVersion: &res.ResourceVersionData{
-		ID:                  11,
-		Version:             "0.1",
-		Description:         "v0.1 Task to run foo",
-		DisplayName:         "foo-bar",
-		MinPipelinesVersion: "0.11",
-		RawURL:              "http://raw.github.url/foo/0.1/foo.yaml",
-		WebURL:              "http://web.github.com/foo/0.1/foo.yaml",
+		ID:                  12,
+		Version:             "0.2",
+		Description:         "Description for task foo-bar version 0.2",
+		DisplayName:         "foo-bar-0.1",
+		MinPipelinesVersion: "0.12",
+		RawURL:              "http://raw.github.url/foo-bar/",
+		WebURL:              "http://web.github.com/foo-bar/",
 		UpdatedAt:           "2020-01-01 12:00:00 +0000 UTC",
 	},
-	Tags: []*res.Tag{
-		{
-			ID:   3,
-			Name: "cli",
-		},
+	Tags: []*res.Tag{},
+}
+
+var ver = &res.Versions{
+	Latest: &res.ResourceVersionData{
+		ID:      12,
+		Version: "0.2",
+		RawURL:  "http://raw.github.url/foo-bar/",
+		WebURL:  "http://web.github.com/foo-bar/",
 	},
 	Versions: []*res.ResourceVersionData{
 		{
-			ID:      11,
+			ID:      13,
 			Version: "0.1",
+			RawURL:  "http://raw.github.url/foo-bar/",
+			WebURL:  "https://github.com/tektoncd/catalog/tree/main/task/buildah/0.1/buildah.yaml",
+		},
+		{
+			ID:      12,
+			Version: "0.2",
+			RawURL:  "http://raw.github.url/foo-bar/",
+			WebURL:  "http://web.github.com/foo-bar/",
 		},
 	},
 }
 
-var pipelineResWithVersion = &res.ResourceVersionData{
-	ID:                  11,
-	Version:             "0.3",
-	Description:         "v0.3 of Pipeline mango.",
+var taskResWithLatestVersion = &res.ResourceVersionData{
+	ID:                  12,
+	Version:             "0.2",
+	Description:         "Description for task foo-bar version 0.2",
 	MinPipelinesVersion: "0.12",
-	RawURL:              "http://raw.github.url/mango/0.3/mango.yaml",
-	WebURL:              "http://web.github.com/mango/0.3/mango.yaml",
+	RawURL:              "http://raw.github.url/foo-bar/",
+	WebURL:              "http://web.github.com/foo-bar/",
 	UpdatedAt:           "2020-01-01 12:00:00 +0000 UTC",
 	Resource: &res.ResourceData{
-		ID:   7,
-		Name: "foo",
-		Kind: "Pipeline",
+		ID:   2,
+		Name: "foo-bar",
+		Kind: "Task",
 		Catalog: &res.Catalog{
 			ID:   1,
-			Name: "fruit",
+			Name: "tekton",
 			Type: "community",
 		},
-		Rating: 4.3,
+		Rating: 4,
 		Tags: []*res.Tag{
 			{
 				ID:   3,
-				Name: "fruit",
+				Name: "foo",
 			},
 		},
 	},
 }
 
-func TestInfoTask_WithoutVersion(t *testing.T) {
-	cli := test.NewCLI()
-
-	defer gock.Off()
-
-	resource := &res.Resource{Data: resource}
-	res := res.NewViewedResource(resource, "default")
-	gock.New(test.API).
-		Get("/resource/tekton/task/foo").
-		Reply(200).
-		JSON(&res.Projected)
-
-	buf := new(bytes.Buffer)
-	cli.SetStream(buf, buf)
-
-	opts := options{
-		cli:  cli,
-		kind: "task",
-		args: []string{"foo"},
-		from: "tekton",
-	}
-
-	err := opts.run()
-	assert.NoError(t, err)
-	golden.Assert(t, buf.String(), fmt.Sprintf("%s.golden", t.Name()))
-	assert.Equal(t, gock.IsDone(), true)
+var taskResWithOldVersion = &res.ResourceVersionData{
+	ID:                  12,
+	Version:             "0.1",
+	Description:         "Description for task foo-bar version 0.1",
+	MinPipelinesVersion: "0.12",
+	RawURL:              "http://raw.github.url/foo-bar/",
+	WebURL:              "http://web.github.com/foo-bar/",
+	UpdatedAt:           "2020-01-01 12:00:00 +0000 UTC",
+	Resource: &res.ResourceData{
+		ID:   2,
+		Name: "foo-bar",
+		Kind: "Task",
+		Catalog: &res.Catalog{
+			ID:   1,
+			Name: "tekton",
+			Type: "community",
+		},
+		Rating: 4,
+		Tags: []*res.Tag{
+			{
+				ID:   3,
+				Name: "foo",
+			},
+		},
+	},
 }
 
-func TestInfoPipeline_WithVersion(t *testing.T) {
+func mockApi(io InfoOptions, taskWithVersion *res.ResourceVersionData) {
+
+	// Get all catalogs
+	gock.New(test.API).
+		Get("/v1/catalogs").
+		Reply(200).
+		JSON(&catalogs)
+
+	// Get all resources with catalog as Tekton
+	rArr := &res.Resources{Data: res.ResourceDataCollection{res2}}
+	resources := res.NewViewedResources(rArr, "withoutVersion")
+	gock.New(test.API).
+		Get("/query").
+		MatchParam("catalogs", io.Catalog).
+		Reply(200).
+		JSON(&resources.Projected)
+
+	// Get ResourceId in order to get all versions of resource
+	rVer := &res.ResourceVersion{Data: taskWithVersion}
+	resWithVersion := res.NewViewedResourceVersion(rVer, "default")
+
+	resInfo := fmt.Sprintf("%s/%s/%s", io.Catalog, io.Kind, io.Name)
+	gock.New(test.API).
+		Get("/resource/" + resInfo).
+		Reply(200).
+		JSON(&resWithVersion.Projected)
+
+	// Get all versios of the resource
+	v := &res.ResourceVersions{Data: ver}
+	version := res.NewViewedResourceVersions(v, "min")
+
+	vArr := fmt.Sprintf("%d/versions", io.ResId)
+	gock.New(test.API).
+		Get("/resource/" + vArr).
+		Reply(200).
+		JSON(&version.Projected)
+
+	gock.New(test.API).
+		Get("/resource/" + resInfo + "/" + io.Version).
+		Reply(200).
+		JSON(&resWithVersion.Projected)
+}
+
+func TestInfoTask_WithLatestVersion(t *testing.T) {
 	cli := test.NewCLI()
 
 	defer gock.Off()
 
-	resVersion := &res.ResourceVersion{Data: pipelineResWithVersion}
-	res := res.NewViewedResourceVersion(resVersion, "default")
-	gock.New(test.API).
-		Get("/resource/fruit/pipeline/mango/0.3").
-		Reply(200).
-		JSON(&res.Projected)
+	mockApi(InfoOptions{
+		ResId:   12,
+		Name:    "foo-bar",
+		Kind:    "task",
+		Catalog: "tekton",
+		Version: "0.2",
+	}, taskResWithLatestVersion)
 
 	buf := new(bytes.Buffer)
 	cli.SetStream(buf, buf)
 
 	opts := options{
 		cli:     cli,
-		kind:    "pipeline",
-		args:    []string{"mango"},
-		from:    "fruit",
-		version: "0.3",
+		kind:    "task",
+		args:    []string{"foo-bar"},
+		from:    "tekton",
+		version: "0.2",
 	}
 
 	err := opts.run()
 	assert.NoError(t, err)
 	golden.Assert(t, buf.String(), fmt.Sprintf("%s.golden", t.Name()))
-	assert.Equal(t, gock.IsDone(), true)
+	assert.Equal(t, gock.IsDone(), false)
 }
 
-func TestInfoPipeline_Description(t *testing.T) {
+func TestInfoTask_WithOldVersion(t *testing.T) {
 	cli := test.NewCLI()
 
 	defer gock.Off()
 
-	pipelineResWithVersion.Description = "Fruits are important sources of vitamins and carbohydrates like fiber and sugar. They are low in calories and naturally sweet. Fruits and their juices are good sources of water, too."
-
-	resVersion := &res.ResourceVersion{Data: pipelineResWithVersion}
-	res := res.NewViewedResourceVersion(resVersion, "default")
-	gock.New(test.API).
-		Get("/resource/fruit/pipeline/mango/0.3").
-		Reply(200).
-		JSON(&res.Projected)
+	mockApi(InfoOptions{
+		ResId:   12,
+		Name:    "foo-bar",
+		Kind:    "task",
+		Catalog: "tekton",
+		Version: "0.1",
+	}, taskResWithOldVersion)
 
 	buf := new(bytes.Buffer)
 	cli.SetStream(buf, buf)
 
 	opts := options{
 		cli:     cli,
-		kind:    "pipeline",
-		args:    []string{"mango"},
-		from:    "fruit",
-		version: "0.3",
+		kind:    "task",
+		args:    []string{"foo-bar"},
+		from:    "tekton",
+		version: "0.1",
 	}
 
 	err := opts.run()
 	assert.NoError(t, err)
 	golden.Assert(t, buf.String(), fmt.Sprintf("%s.golden", t.Name()))
-	assert.Equal(t, gock.IsDone(), true)
+	assert.Equal(t, gock.IsDone(), false)
 }
 
 func TestPipelineTask_MultiLineDescription(t *testing.T) {
@@ -180,28 +260,29 @@ func TestPipelineTask_MultiLineDescription(t *testing.T) {
 
 	defer gock.Off()
 
-	pipelineResWithVersion.Description = "Fruits are important sources of vitamins and carbohydrates like fiber and sugar. \nThey are low in calories and naturally sweet. \nFruits and their juices are good sources of water, too."
+	taskResWithLatestVersion.Description = "A Task is a collection of Steps that you define and arrange in a specific order of execution as part of your continuous integration flow. A Task executes as a Pod on your Kubernetes cluster. A Task is available within a specific namespace, while a ClusterTask is available across the entire cluster."
 
-	resVersion := &res.ResourceVersion{Data: pipelineResWithVersion}
-	res := res.NewViewedResourceVersion(resVersion, "default")
-	gock.New(test.API).
-		Get("/resource/fruit/pipeline/mango/0.3").
-		Reply(200).
-		JSON(&res.Projected)
+	mockApi(InfoOptions{
+		ResId:   12,
+		Name:    "foo-bar",
+		Kind:    "task",
+		Catalog: "tekton",
+		Version: "0.2",
+	}, taskResWithLatestVersion)
 
 	buf := new(bytes.Buffer)
 	cli.SetStream(buf, buf)
 
 	opts := options{
 		cli:     cli,
-		kind:    "pipeline",
-		args:    []string{"mango"},
-		from:    "fruit",
-		version: "0.3",
+		kind:    "task",
+		args:    []string{"foo-bar"},
+		from:    "tekton",
+		version: "0.2",
 	}
 
 	err := opts.run()
 	assert.NoError(t, err)
 	golden.Assert(t, buf.String(), fmt.Sprintf("%s.golden", t.Name()))
-	assert.Equal(t, gock.IsDone(), true)
+	assert.Equal(t, gock.IsDone(), false)
 }
