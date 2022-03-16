5a18c384bf637751ea152af7159d5e8adbb72ce4
pratap0007 <shverma@redhat.com>
2022-03-16 21:58:27 +0530

    Improves UI redirection after successful authentication
    This patch improves ui redirection by redirecting users to the same page
    where they had attempted to login after successful authentication
    
    Signed-off-by: Shiv Verma <shverma@redhat.com>

--- ui/src/containers/Header/index.tsx
+++ ui/src/containers/Header/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { observer } from 'mobx-react';
 import { useHistory } from 'react-router-dom';
 import {
@@ -31,12 +31,56 @@ import { AUTH_BASE_URL, REDIRECT_URI } from '../../config/constants';
 import { IProvider } from '../../store/provider';
 import { titleCase } from '../../common/titlecase';
 import AlertDisplay from '../../components/AlertDisplay';
+import { AuthCodeProps, IError } from '../../store/auth';
 
 const Header: React.FC = observer(() => {
   const { user, providers } = useMst();
   const history = useHistory();
   const [isModalOpen, setIsModalOpen] = React.useState(false);
   const [disable, setDisable] = React.useState(false);
+
+  const detailPagePath = `detailPagePath`;
+
+  useEffect(() => {
+    // if user tries to authenticate from detail page
+    // Redirects to resource detail page
+    if (localStorage.getItem(detailPagePath) !== null && user.isAuthenticated === true) {
+      history.replace(`${localStorage.getItem(detailPagePath)}`);
+      localStorage.removeItem(detailPagePath);
+    }
+  }, [user.isAuthenticated]);
+
+  useEffect(() => {
+    // Parse the url and read the status and code params
+    const searchParams = new URLSearchParams(window.location.search);
+    const status = searchParams.get('status');
+    const code = searchParams.get('code');
+
+    // It checks status and code and then redirect to authentication
+    if (status === '200' && code !== null) {
+      const codeFinal: AuthCodeProps = {
+        code: code
+      };
+      user.authenticate(codeFinal);
+    }
+
+    // Strore the resource detail page path in the localStorage
+    if (history.location.pathname !== '/' && !user.isAuthenticated) {
+      localStorage.setItem(detailPagePath, history.location.pathname);
+    }
+
+    if (!user.isAuthenticated && status !== '200' && status !== null) {
+      // Wait to redirection of page and then update the store
+      setTimeout(() => {
+        const error: IError = {
+          status: Number(status),
+          serverMessage: 'Login Failed, Please Try To Login Again!',
+          customMessage: ''
+        };
+        user.setErrorMessage(error);
+      }, 1000);
+    }
+  }, [window.location.href]);
 
   const headerTools = (
     <PageHeaderTools>
--- ui/src/containers/ParseUrl/index.tsx
+++ ui/src/containers/ParseUrl/index.tsx
@@ -1,40 +1,13 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 import { useMst } from '../../store/root';
 import { Params } from '../../common/params';
-import { AuthCodeProps, IError } from '../../store/auth';
 
 const ParseUrl: React.FC = () => {
-  const { resources, user } = useMst();
-  const history = useHistory();
+  const { resources } = useMst();
 
   if (window.location.search) {
-    const searchParams: URLSearchParams = new URLSearchParams(window.location.search);
-    const status = searchParams.get('status');
-    const code = searchParams.get('code');
+    const searchParams = new URLSearchParams(window.location.search);
 
-    // It checks status and code and then redirect to authentication
-    if (status === '200' && code !== null) {
-      const codeFinal: AuthCodeProps = {
-        code: code
-      };
-      user.authenticate(codeFinal);
-      if (user.isAuthenticated) {
-        history.goBack();
-      }
-    }
-    // Display the alert message when status is not ok
-    else if (!user.isAuthenticated && status !== '200' && status !== null) {
-      // Wait to redirection of page and then update the store
-      setTimeout(() => {
-        const error: IError = {
-          status: Number(status),
-          serverMessage: 'Login Failed, Please Try To Login Again!',
-          customMessage: ''
-        };
-        user.setErrorMessage(error);
-      }, 1000);
-    }
     if (searchParams.has(Params.Query)) {
       resources.setSearch(searchParams.get(Params.Query) || '');
     }
