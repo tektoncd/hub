600d092dbc54f1f45644ccd62787660aeeacfe00
PuneetPunamiya <ppunamiy@redhat.com>
2022-08-25 14:43:35 +0530

    Fixes react test renderer by bumping the react version
    Signed-off-by: Puneet Punamiya ppunamiy@redhat.com

--- ui/src/components/Background/Background.test.tsx
+++ ui/src/components/Background/Background.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Background from '.';
 
-it('should render the background component', () => {
+fit('should render the background component', () => {
   const component = renderer.create(<Background />).toJSON();
   expect(component).toMatchSnapshot();
 });
