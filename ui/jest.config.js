b58129cd65451903e340e98d662e72c06e229adf
PuneetPunamiya <ppunamiy@redhat.com>
2023-01-31 11:37:58 +0530

    Updates react-markdown to 8.0.5
    Signed-off-by: Puneet Punamiya ppunamiy@redhat.com

---
+++ ui/jest.config.js
@@ -0,0 +1,36 @@
+const esModules = [
+  'react-markdown',
+  'vfile',
+  'unist-.+',
+  'unified',
+  'bail',
+  'is-plain-obj',
+  'trough',
+  'remark-.+',
+  'mdast-util-.+',
+  'micromark',
+  'parse-entities',
+  'character-entities',
+  'property-information',
+  'comma-separated-tokens',
+  'hast-util-whitespace',
+  'remark-.+',
+  'space-separated-tokens',
+  'decode-named-character-reference',
+  'ccount',
+  'escape-string-regexp',
+  'markdown-table',
+].join('|');
+
+module.exports = {
+  moduleNameMapper: {
+    // ...
+  },
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '\\.[jt]sx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    `[/\\\\]node_modules[/\\\\](?!${esModules}).+\\.(js|jsx|mjs|cjs|ts|tsx)$`,
+  ],
+};
