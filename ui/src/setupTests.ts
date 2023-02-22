f8aea6bbc2203fa86e4e18b0c073e4e75d963b7b
pratap0007 <shverma@redhat.com>
2023-02-23 01:03:55 +0530

    Bump react-dom to 17.0.0 in UI
    Signed-off-by: Shiv Verma <shverma@redhat.com>

--- ui/src/setupTests.ts
+++ ui/src/setupTests.ts
@@ -2,8 +2,7 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
 
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 configure({ adapter: new Adapter() });
