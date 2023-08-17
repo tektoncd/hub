6738874ee13c88d438e2ca8405b752940ca6fdd2
PuneetPunamiya <ppunamiy@redhat.com>
2023-08-17 18:07:25 +0530

    Fix lint errors related to prettier in UI
    Signed-off-by: Puneet Punamiya ppunamiy@redhat.com

--- ui/src/setupTests.ts
+++ ui/src/setupTests.ts
@@ -4,5 +4,5 @@
 // learn more: https://github.com/testing-library/jest-dom
 
 import { configure } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 configure({ adapter: new Adapter() });
