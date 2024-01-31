9cc74543ef435c55c97531c8fadf609dc5fdb9fa
PuneetPunamiya <ppunamiy@redhat.com>
2024-01-31 18:48:47 +0530

    Fixes swagger build and adds swagger build check in CI
    Signed-off-by: PuneetPunamiya <ppunamiy@redhat.com>

--- swagger/src/index.tsx
+++ swagger/src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
   <React.Fragment>
     <App />
-  </React.Fragment>,
-  document.getElementById('root')
+  </React.Fragment>
 );
