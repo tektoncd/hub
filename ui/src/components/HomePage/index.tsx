81bd06702e2e5ea3428ea5c7a77d6404e8d0f354
pratap0007 <shverma@redhat.com>
2025-08-20 22:25:03 +0530

    Move the banner of deprecation notice to HomePage
    This PR will move the banner from Header to HomePage
    as it was blocking the page scrolling functionality
    
    Signed-off-by: Shiv Verma <shverma@redhat.com>

--- ui/src/components/HomePage/index.tsx
+++ ui/src/components/HomePage/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Grid, GridItem, PageSection } from '@patternfly/react-core';
+import {
+  Banner,
+  Grid,
+  GridItem,
+  PageSection,
+  TextContent,
+  TextVariants,
+  Text
+} from '@patternfly/react-core';
 import LeftPane from '../LeftPane';
 import Background from '../Background';
 import Resources from '../../containers/Resources';
-
+import './HomePage.css';
 const HomePage = () => {
   return (
     <>
+      <Banner variant="warning" isSticky={true}>
+        <TextContent>
+          <Text component={TextVariants.h1} className="hub-banner-text">
+            Tekton Hub is deprecated and will be shutdown on January 2026, the 7th.
+          </Text>
+        </TextContent>
+      </Banner>
       <Background />
       <PageSection>
         <Grid hasGutter>
