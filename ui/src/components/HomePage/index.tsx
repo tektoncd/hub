7206b8061e758eee9e5511c3e2d3015d70bec467
shverma <shverma@redhat.com>
2025-12-01 21:52:24 +0530

    Remove Hub deprecation banner notice

--- ui/src/components/HomePage/index.tsx
+++ ui/src/components/HomePage/index.tsx
@@ -1,27 +1,10 @@
-import React from 'react';
-import {
-  Banner,
-  Grid,
-  GridItem,
-  PageSection,
-  TextContent,
-  TextVariants,
-  Text
-} from '@patternfly/react-core';
-import LeftPane from '../LeftPane';
-import Background from '../Background';
+import { Grid, GridItem, PageSection } from '@patternfly/react-core';
 import Resources from '../../containers/Resources';
-import './HomePage.css';
+import Background from '../Background';
+import LeftPane from '../LeftPane';
 const HomePage = () => {
   return (
     <>
-      <Banner variant="warning" isSticky={true}>
-        <TextContent>
-          <Text component={TextVariants.h1} className="hub-banner-text">
-            Tekton Hub is deprecated and will be shutdown on January 2026, the 7th.
-          </Text>
-        </TextContent>
-      </Banner>
       <Background />
       <PageSection>
         <Grid hasGutter>
