c29a0ff225464710e92c71267223cece0dd18cda
shverma <shverma@redhat.com>
2026-01-07 12:31:24 +0530

    Fix to mock both Date and Date.mow()
    This fixes mock Date in the test and snapshots
    
    Signed-off-by: Shiv Verma <shverma@redhat.com>

--- ui/src/common/testutils.ts
+++ ui/src/common/testutils.ts
@@ -1,13 +1,18 @@
-//Sets a Fake date for test
+const RealDate = global.Date;
+const FIXED = new RealDate('2026-01-01T10:20:30Z');
+
 export const FakeDate = () => {
   beforeEach(() => {
-    global.Date.now = jest.fn(() => new Date('2021-01-01T10:20:30Z').getTime());
+    global.Date = class extends RealDate {
+      constructor(...args: [number?]) {
+        super(args[0] ?? FIXED.getTime());
+      }
+      static now = () => FIXED.getTime();
+    } as DateConstructor;
+  });
+  afterEach(() => {
+    global.Date = RealDate;
   });
 };
 
-//Sets an Actual date for test
-export const ActualDate = () => {
-  afterEach(() => {
-    global.Date = Date;
-  });
-};
+export const ActualDate = () => {};
